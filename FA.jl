25644a300a201c1cef80d397b86bbb70a8c2a3bb
Squalm <43246103+Squalm@users.noreply.github.com>
2020-09-15 19:09:40 +0100

    A few hours more work and very little progress! Whooopie...

--- FA.jl
+++ FA.jl
@@ -1,17 +1,23 @@
 # Get user input - TBDepricated
-println()
-print("Text: ")
+# println("This is here to be eaten by the console.")
+println("Text: ")
 text = readline()
 print("Proceed with Frequency Analysis? [Press Enter]")
 readline()
 
 # Default alphabet - TBDepricated
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet_array = Array{String}(undef, length(alphabet))
+for c in range(1, length = length(alphabet))
+    alphabet_array = c
+end
+# println(typeof(alphabet))
 
 # This is all the code we need to count all the stuff. Comprehension?
-counted_dict = {any[]}
-for i in length(alphabet)
-    counted_dict[alphabet[i]] = count(alphabet[i], text)
+counted_dict = ([])
+
+for i in range(1, length = length(alphabet_array))
+    counted_dict[alphabet_array[i]] = count(alphabet_array[i], text)
 end
 
 # Unsorted out.
