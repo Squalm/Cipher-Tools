8bde9b3de9bf204619bac8124ecb46e01d4113b2
Squalm <43246103+Squalm@users.noreply.github.com>
2020-09-16 09:05:19 +0100

    Put the whole thing in a function and additional cleanliness.

--- FA.jl
+++ FA.jl
@@ -1,22 +1,24 @@
-# Get user input - TBDepricated
-println("Text: ")
-text = readline()
-print("Proceed with Frequency Analysis? [Press Enter]")
-readline()
+function fa(input, alphabet)
 
-# Default alphabet - TBDepricated
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-alphabet_array = split(alphabet, "")
+    text = input
+    alphabet_array = split(alphabet, "")
 
-# Count values in text.
-counted = Dict()
-for i in alphabet_array
-    counted[i] = count(i, text) / length(text)
+    # Count values in text.
+    counted = Dict()
+    for i in alphabet_array
+        counted[i] = count(i, text) / length(text)
+    end
+
+    # Sorted out.
+    println("")
+    println("Count [Sorted by size]:")
+    for i in sort!(collect(counted), by = x->x[2], rev = true)
+        println(i)
+    end
 end
 
-# Sorted out.
-println("")
-println("Count [Sorted by size]:")
-for i in sort!(collect(counted), by = x->x[2], rev = true)
-    println(i)
+while true
+    println("")
+    println("Text:")
+    fa(readline(), "abcdefghijklmnopqrstuvwxyz")
 end
