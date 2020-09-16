6a59d8dbb83fc61a384a584de8a1a1dcabb75a42
Squalm <43246103+Squalm@users.noreply.github.com>
2020-09-16 20:39:38 +0100

    Tydied up.

--- FAhumans.jl
+++ FAhumans.jl
@@ -2,21 +2,18 @@ include("individual\\FAfunc.jl")
 
 while true
 
-    println("")
-    println("Text [leave blank to break]:")
+    println("\nText [leave blank to break]:")
     human_input = readline()
     human_input == "" ? break :
-    println("Alphabet [leave blank for default]:")
+    println("\nAlphabet [leave blank for default]:")
     human_alphabet = readline()
     human_alphabet == "" ? human_alphabet = "abcdefghijklmnopqrstuvwxyz" :
-    println("")
-    println("Frequency Analysis: ")
+    println("\nFrequency Analysis: ")
     counted = fa(human_input, human_alphabet)
     for i in sort!(collect(counted), by = x->x[2], rev = true)
         println(i)
     end
-    println("")
-    println("Index of Coincidence: ")
+    println("\nIndex of Coincidence: ")
     println(coincidence(human_input, split(human_alphabet, "")))
 
 end
