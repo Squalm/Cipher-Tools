ca9dbfe93ffb45e53f56b926b725f389d30cd2a7
Squalm <43246103+Squalm@users.noreply.github.com>
2020-09-16 08:50:30 +0100

    Cleaned up the code a little

--- FA.jl
+++ FA.jl
@@ -1,5 +1,4 @@
 # Get user input - TBDepricated
-# println("This is here to be eaten by the console.")
 println("Text: ")
 text = readline()
 print("Proceed with Frequency Analysis? [Press Enter]")
@@ -9,25 +8,15 @@ readline()
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 alphabet_array = split(alphabet, "")
 
-# println(typeof(alphabet))
-
-# This is all the code we need to count all the stuff. Comprehension?
+# Count values in text.
 counted = Dict()
-# count = []
-
 for i in alphabet_array
     counted[i] = count(i, text) / length(text)
 end
 
-# Unsorted out.
-# println("Count:")
-# println(sort!(collect(counted), by = x->x[1]))
-
 # Sorted out.
 println("")
 println("Count [Sorted by size]:")
-# println(sort!(collect(counted), by = x->x[2], rev = true))
-
 for i in sort!(collect(counted), by = x->x[2], rev = true)
     println(i)
 end
