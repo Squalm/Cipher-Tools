a70f269337e2a206aad640dce14d7c4cb9f2ce2d
Squalm <43246103+Squalm@users.noreply.github.com>
2020-09-19 17:52:08 +0100

    Caesar Cipher? Well, not yet... but still.

--- individual/Caesarfunc.jl
+++ individual/Caesarfunc.jl
@@ -1,7 +1,14 @@
 # Caesar shift function
 function caesarshift(text, alphabet, shift)
 
-    numbers = []
-    
+    numbers = Array{length(alphabet), Int64}(0, 1)
+    shifted = ""
+    for i in range(1, length = length(text))
+        numbers[i] = findfirst(x -> x == text[i], alphabet)
+    end
+
+    return shifted
 
 end
+
+println(caesarshift(readline(), "abcdefghijklmnopqrstuvwxyz", 2))
