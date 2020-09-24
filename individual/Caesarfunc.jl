849bfa8ea8a28188355678aea0baa5661ac23d6b
Squalm <43246103+Squalm@users.noreply.github.com>
2020-09-24 18:16:09 +0100

    Removed testing human out from caesarfunc

--- individual/Caesarfunc.jl
+++ individual/Caesarfunc.jl
@@ -31,19 +31,3 @@ function caesarbrute(text, alphabet)
     return shifts
 
 end
-
-while true
-
-    println("\nText to shift [leave blank to break]: ")
-    human_input = readline()
-    human_input == "" ? break :
-    println("\nAlphabet: ")
-    human_alphabet = readline()
-
-    shifts = caesarbrute(human_input, human_alphabet)
-
-    for item in range(1, length = length(shifts))
-        println("Shift of ", (item + 1) % length(shifts), ": ", shifts[item])
-    end
-
-end
