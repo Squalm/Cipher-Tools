01eae2a7d7d9805150b1742f0ed40ac8c90d5940
Squalm <43246103+Squalm@users.noreply.github.com>
2020-10-01 09:45:16 +0100

    More code tyding; fewer variables etc.

--- individual/Caesarfunc.jl
+++ individual/Caesarfunc.jl
@@ -3,16 +3,13 @@ function caesarshift(text, alphabet, shift)
 
     # Turn to numbers
     numbers = []
-    shifted = ""
     for letter in text
         num = findfirst(isequal(letter), alphabet)
         num == nothing ? println("Blank found.") : push!(numbers, num)
     end
 
     # Shift nmubers
-    shifted = [alphabet[i] for i in [((x + shift) % length(alphabet)) + 1 for x in numbers]]
-
-    return shifted
+    return [alphabet[i] for i in [((x + shift) % length(alphabet)) + 1 for x in numbers]]
 
 end
 
