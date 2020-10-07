643addaccd6cf2204ef7d02861cb86bab38ed6e7
Squalm <43246103+Squalm@users.noreply.github.com>
2020-10-07 11:21:59 +0100

    Default alphabet

--- Caesarhumans.jl
+++ Caesarhumans.jl
@@ -5,8 +5,10 @@ while true
     println("\nText to shift [leave blank to break]: ")
     human_input = readline()
     human_input == "" ? break :
-    println("\nAlphabet: ")
+    println("\nAlphabet [leave blank for default]: ")
     human_alphabet = readline()
+    human_alphabet == "" ? human_alphabet = "abcdefghijklmnopqrstuvwxyz" :
+    println()
 
     shifts = caesarshift.(human_input, human_alphabet, 1:length(human_alphabet))
 
