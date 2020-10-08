072e9c020c45c81a5f2734b4dd25d1e5f9a52729
Squalm <43246103+Squalm@users.noreply.github.com>
2020-10-08 09:48:10 +0100

    Overloaded caesarshift to return a string

--- individual/Caesarfunc.jl
+++ individual/Caesarfunc.jl
@@ -8,3 +8,16 @@ function caesarshift(text::String, alphabet::String, shift::Int)
     return [alphabet[i + 1] for i in [((x + shift) % length(alphabet)) for x in numbers]]
 
 end # function
+
+function caesarshift(text::String, alphabet::String, shift::Int, returnStr::Bool)
+    if returnStr == false
+        return caesarshift(text, alphabet, shift)
+    else
+        outarr = caesarshift(text, alphabet, shift)
+        out = ""
+        for i in outarr
+            out *= i
+        end # for
+        return out
+    end # if
+end # function
