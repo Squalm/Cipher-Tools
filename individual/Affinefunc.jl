61fc40cd43b9ef0c316c43feb88c1502114d968a
Squalm <43246103+Squalm@users.noreply.github.com>
2020-10-12 18:20:06 +0100

    Working Affine single substitution now

--- individual/Affinefunc.jl
+++ individual/Affinefunc.jl
@@ -3,16 +3,18 @@ include("Utils.jl")
 # Affine Substitute
 function affine(text::String, alphabet::String, nMultiply::Int, nAdd::Int)
 
-    out = join([findfirst(isequal(letter), alphabet) for letter in text])
+    return join([alphabet[(findfirst(isequal(letter), alphabet) * nMultiply + nAdd) % length(alphabet) + 1] for letter in text])
 
 end # function
 
 function affinebrute(text::String, alphabet::String)
 
-    text = 
+    text = removeExtras(text, alphabet)
 
     for i in alphabet
 
     end
 
 end # function
+
+println(affine("abc", "abcde", 2, 3))
