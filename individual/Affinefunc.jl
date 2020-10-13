b8ff87c6ab027557c1365d1106abbbe8b71483c5
Squalm <43246103+Squalm@users.noreply.github.com>
2020-10-13 18:38:35 +0100

    Most of affine

--- individual/Affinefunc.jl
+++ individual/Affinefunc.jl
@@ -1,9 +1,17 @@
 include("Utils.jl")
+include("FAfunc.jl")
 
 # Affine Substitute
 function affine(text::String, alphabet::String, nMultiply::Int, nAdd::Int)
 
     return join([alphabet[(findfirst(isequal(letter), alphabet) * nMultiply + nAdd) % length(alphabet) + 1] for letter in text])
+
+end # function
+
+# Coprime Calculator
+function coprime(n::Int)
+
+    return [i for i in 2:n if gcd(i, n) == 1]
 
 end # function
 
@@ -13,8 +21,6 @@ function affinebrute(text::String, alphabet::String)
 
     for i in alphabet
 
-    end
+    end # for
 
 end # function
-
-println(affine("abc", "abcde", 2, 3))
