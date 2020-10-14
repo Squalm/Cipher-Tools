21a8f4767183bd616e6a70b5f7bcd7cc84f1155b
Squalm <43246103+Squalm@users.noreply.github.com>
2020-10-14 11:52:45 +0100

    formatted a little too hard, unformatting

--- programs/Caesarfunc.jl
+++ programs/Caesarfunc.jl
@@ -4,11 +4,9 @@ function caesarshift(text::String, alphabet::String, shift::Int)
     # Shift nmubers
     return join([
         alphabet[i+1]
-        for
-        i in [
+        for i in [
             ((x + shift) % length(alphabet))
-            for
-            x in [
+            for x in [
                 findfirst(isequal(letter), alphabet)
                 for letter in text if occursin(letter, alphabet)
             ]
--- programs/Entropyfunc.jl
+++ programs/Entropyfunc.jl
@@ -5,8 +5,7 @@ function entropy(text::String, alphabet::String)
     refined = removeExtras(text, alphabet)
     return sum([
         (-i) * log2(i)
-        for
-        i in [
+        for i in [
             count(string(letter), refined) / length(refined)
             for letter in alphabet if occursin(letter, text)
         ]
