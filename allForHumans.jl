41a1d0c24a83692e3e8babec1d1d8f5b9f285a17
Squalm <43246103+Squalm@users.noreply.github.com>
2020-10-23 19:56:18 +0100

    Initial guide

---
+++ allForHumans.jl
@@ -0,0 +1,24 @@
+# Allow selection of different modules - TO DO INTEGRATE EACH MODEL INTO HERE
+
+while true
+
+    print("""
+
+--- CIPHER TOOLS > ALL FOR HUMANS ---
+
+HELP - ?COMMAND
+
+ANALYSIS
+| -> Entropy - EN
+| -> Frequency Analysis - FA
+
+BRUTE FORCE
+| -> Caesar Brute - CA
+| -> Affine Brute - AF
+
+[END]
+
+>> """)
+    text = readline()
+
+end
