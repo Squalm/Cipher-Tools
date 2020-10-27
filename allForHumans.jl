21335cd0cd630fca713fee0ee7d79bfa38ea0a23
Squalm <43246103+Squalm@users.noreply.github.com>
2020-10-27 16:53:17 +0000

    Done!

--- allForHumans.jl
+++ allForHumans.jl
@@ -1,12 +1,11 @@
 # Allow selection of different modules - TO DO INTEGRATE EACH MODEL INTO HERE
 
-while true
-
-    print("""
+help = """
 
 --- CIPHER TOOLS > ALL FOR HUMANS ---
 
-HELP - ?COMMAND
+Help - ?
+Quit - Q
 
 ANALYSIS
 | -> Entropy - EN
@@ -16,9 +15,47 @@ BRUTE FORCE
 | -> Caesar Brute - CA
 | -> Affine Brute - AF
 
-[END]
+"""
 
->> """)
+print(help)
+
+while true
+
+    print(">> ")
     text = readline()
+
+    if uppercase(text) == "EN"
+
+        println("\n--- CIPHER TOOLS > ENTROPY HUMANS ---")
+        include("Entropyhumans.jl")
+
+    elseif uppercase(text) == "FA"
+
+        println("\n--- CIPHER TOOLS > FREQUENCY ANALYSIS HUMANS ---")
+        include("FAhumans.jl")
+
+    elseif uppercase(text) == "CA"
+
+        println("\n--- CIPHER TOOLS > CAESAR HUMANS ---")
+        include("Caesarhumans.jl")
+
+    elseif uppercase(text) == "AF"
+
+        println("\n--- CIPHER TOOLS > AFFINE HUMANS ---")
+        include("Affinehumans.jl")
+
+    elseif uppercase(text) == "?"
+
+        print(help)
+
+    elseif uppercase(text) == "Q"
+
+        break
+
+    else
+
+        println("Not a recognised code.")
+
+    end
 
 end
