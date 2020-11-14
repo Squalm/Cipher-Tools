30b502bc6c3feb055c91532f58c6c7cab4b59c29
Squalm <43246103+Squalm@users.noreply.github.com>
2020-11-14 10:23:51 +0000

    Create Requirements.jl

---
+++ Requirements.jl
@@ -0,0 +1,6 @@
+import Pkg
+# This file will import all necessary Packages for cipher-tools.
+
+# Make sure user really wants to do this
+println("This Programs installs packages required for Cipher-Tools. Press enter to begin the process.")
+readline()
