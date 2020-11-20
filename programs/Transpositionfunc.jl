25d84bb4badbaeee5f0b171fb92b835594ab2546
Squalm <43246103+Squalm@users.noreply.github.com>
2020-11-20 09:49:20 +0000

    enfiddle

--- programs/Transpositionfunc.jl
+++ programs/Transpositionfunc.jl
@@ -1,18 +1,17 @@
 include("Utils.jl")
 include("FAfunc.jl")
 
-# Rail Fence Cipher
+# Scytale Cipher
 function scytale(text::String, rails::Integer)
 
     # Assumes that the text is already clean
     # Since this is a permutation cipher it doesn't need an alphabet
 
-    out = []
     eachrail = Dict()
     for i in range(1, length = rails)
         eachrail[i] = join([text[x] for x in range(1, length = length(text)) if x % rails - i == 0 || x % rails - i == -rails])
     end
-    println(eachrail)
+    # println(eachrail)
 
     #==
     for i in range(1, length = rails)
