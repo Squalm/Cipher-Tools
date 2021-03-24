024b36bfb789350aa6cc3d655723718d0b81a7ed
Squalm <43246103+Squalm@users.noreply.github.com>
2021-03-24 15:54:50 +0000

    progress bacon (1)

--- programs/Baconfunc.jl
+++ programs/Baconfunc.jl
@@ -8,7 +8,18 @@ Takes `text` and substitutes binary `outchars` (2 characters) according to `alph
 """
 function bacon(text::String, alphabet::String, outchars::String)
 
+    _0 = outchars[1]
+    _1 = outchars[2]
+    outarr = []
 
+    for char in text
+
+
+
+    end #  for
+
+    out = ""
+    return push!(out, outarr)
 
 end # function
 
