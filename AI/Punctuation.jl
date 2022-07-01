711b03be74f5ed7fff5e7a5dd01ea4c84e086f6b
Squalm <43246103+Squalm@users.noreply.github.com>
2022-07-01 14:12:37 +0100

    breaking substitution ciphers

--- AI/Punctuation.jl
+++ AI/Punctuation.jl
@@ -1,1 +1,33 @@
-#== In theory this is a not too complicated RNN to add punctuation into some text ==#
+#== In theory this is a not too complicated NN to add punctuation into some text ==#
+
+module PunctuateM
+
+export punctuate
+
+using Main.UtilsM: removeExtras
+
+function create_training(texts)
+
+    for file in texts
+
+        text = ""
+        open(file) do f
+            while !eof(f)
+                text *= readline(f)
+            end # while            
+        end # do
+
+        
+
+    end # for
+
+end # function
+
+function punctuate()
+    
+end # function
+
+end # module
+
+# Tests
+create_training(["Texts/bookbits/mobydick.txt"])
--- programs/Substitutefunc.jl
+++ programs/Substitutefunc.jl
@@ -2,24 +2,78 @@ module SubstituteM
 
 export substituteGuess
 
-# include("Utils.jl")
-# include("FAfunc.jl")
-# include("comparisons.jl")
-
 using Main.UtilsM: removeExtras
-using Main.FAM: fa
+using Main.FAM: fa, bigramScore, bigramFreqs
 using Main.ComparisonM: faComparison
+using ProgressBars
 
-# Susbistution guess - ONLY WORKS WITH ENGLISH
+"""
+    substitute(text::String, alphabet::String, newalphabet::String)
+
+Substitues `alphabet` for `newalphabet` in `text`.
+"""
+function substitute(text::String, alphabet::String, newalphabet::String)
+
+    return join([newalphabet[findfirst(c, alphabet)] for c in text])
+
+end # function
+
+"""
+    substituteGuess(text::String)
+
+Only works for English text.
+
+Compares the text with usually English frequency analysis and
+attempts to guess the key based on that.
+
+N.B. This does not usually actually create recognisable text.
+"""
 function substituteGuess(text::String)
 
-    engfreq = facomparison()
-    textfreq = fa(uppercase(text), "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    sortedeng = sort(collect(engfreq), by = x -> x[2])
-    sortedtext = sort(collect(textfreq), by = x -> x[2])
-    for i in [1:sortedeng]
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    text = removeExtras(uppercase(text), alphabet)
+    alphabet = join([t[1] for t in sort(collect(faComparison()), by = x -> x[2], rev = true)])
+    newalphabet = join([t[1] for t in sort(collect(fa(text, alphabet)), by = x -> x[2], rev = true)])
+    return (text = substitute(text, newalphabet, alphabet), freq = (alphabet, newalphabet))
 
-    end
+end # function
+
+function substituteBrute(text::String, swaps = 10000, alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    text = removeExtras(uppercase(text), alphabet)
+
+    text, freq = substituteGuess(text)
+    text = [c for c in text]
+
+    # Use bigrams analysis to get a better solution
+    # Read the whole file into a dictionary for easy access
+    bigrams = bigramFreqs()
+
+    best = (freq[2], bigramScore(join(text), bigrams))
+
+    bar = ProgressBar(1:swaps)
+    set_description(bar, best[1])
+
+    for _ in bar
+
+        swap = (rand(1:length(best[1])), rand(1:length(best[1])))
+        new_key = [c for c in best[1]]
+        new_key[swap[1]], new_key[swap[2]] = new_key[swap[2]], new_key[swap[1]]
+
+        new_key = join(new_key)
+
+        new_score = bigramScore(join(substitute(join(text), freq[2], new_key)), bigrams)
+
+        if new_score > best[2]
+            best = (new_key, new_score)
+            set_description(bar, best[1])
+        end # if
+
+    end # for
+
+    text = substitute(join(text), freq[2], best[1])
+
+    return (text = join(text), old = freq[2], key = best[1])
 
 end # function
 
